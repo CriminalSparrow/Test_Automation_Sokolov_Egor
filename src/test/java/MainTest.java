804b5d73a7d931b822c4db21b866dd8110388b9e
CriminalSparrow <socolovich4@gmail.com>
2025-03-20 16:58:27 +0300

    First commit?

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, Test!");
+    }
+
+    @Test
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
